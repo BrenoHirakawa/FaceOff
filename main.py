bbc4a0600b1af8c50b430de51418298758a6b010
BrenoHirakawa <breno.hiroyuki@unifei.edu.br>
2026-06-03 23:19:46 -0300

    mudanças na main, adição de novas funções

--- main.py
+++ main.py
@@ -1,45 +1,77 @@
 import cv2
-from ultralytics import YOLO
+import time
+import requests 
+import onnxruntime as ort
+import numpy as np
 
-# Carrega modelo leve (nano)
-model = YOLO("yolov8n.pt")
+FIREBASE_URL = "https://bim-detector-default-rtdb.firebaseio.com/status.json"
 
-# Inicializa câmera
+session = ort.InferenceSession(
+    "models/yolov8n.onnx",
+    providers=["CPUExecutionProvider"]
+)
+
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Erro ao acessar câmera")
     exit()
 
-# (Importante pra Raspberry) reduzir resolução
 cap.set(3, 640)
 cap.set(4, 480)
 
+ultimo_envio = 0
+INTERVALO_ENVIO = 5 
+
+
 while True:
+
     ret, frame = cap.read()
+
     if not ret:
         break
 
-    # Inferência (tamanho menor = mais rápido)
-    results = model(frame, imgsz=320, conf=0.4, verbose=False)
-
     pessoas = 0
 
-    for r in results:
-        for box in r.boxes:
-            cls = int(box.cls[0])
+    cv2.putText(
+        frame,
+        f"Pessoas: {pessoas}",
+        (10, 30),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2
+    )
 
-            # Classe 0 = pessoa (COCO dataset)
-            if cls == 0:
-                pessoas += 1
+    cv2.imshow("FaceOff", frame)
 
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    agora = time.time()
 
-    # Mostrar contagem
-    cv2.putText(frame, f'Pessoas: {pessoas}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    if agora - ultimo_envio >= INTERVALO_ENVIO:
 
-    cv2.imshow("YOLOv8 - Monitoramento", frame)
+        dados = {
+            "quantidade": pessoas,
+            "ambiente": "laboratorio",
+            "timestamp": int(agora)
+        }
+
+        try:
+
+            response = requests.put(
+                FIREBASE_URL,
+                json=dados,
+                timeout=5
+            )
+
+            print(
+                f"[FIREBASE] {response.status_code} | Pessoas: {pessoas}"
+            )
+
+        except Exception as e:
+
+            print(f"Erro ao enviar: {e}")
+
+        ultimo_envio = agora
 
     if cv2.waitKey(1) == 27:
         break
